c89433f308e48e898c89a0b95520fddcdf652d62
Roman Patsev <rpatsev@gmail.com>
2018-03-30 00:07:03 +0300

    linqtask uploaded

---
+++ LinqTask/Actor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTask
+{
+    class Actor
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+    abstract class ArtObject
+    {
+
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+    class Film : ArtObject
+    {
+
+        public int Length { get; set; }
+        public IEnumerable<Actor> Actors { get; set; }
+    }
+    class Book : ArtObject
+    {
+
+        public int Pages { get; set; }
+    }
+}
+
